0ff8c5ca2f165dd6bf3693e39bf631e345747127
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-12 22:05:02 +0200

    # Querying related collections - Get All Posts by UserName

--- back-end/controller/posts.js
+++ back-end/controller/posts.js
@@ -13,3 +13,17 @@ exports.getAllPosts = async (req, res, next) => {
     })
     .catch((error) => next(error));
 };
+
+//Get posts by userName (user name on params)
+exports.getPostsByUserName = async (req, res, next) => {
+  const { username } = req.params;
+  Posts.find({ username })
+    .then((postsArray) => {
+      if (postsArray.length === 0) {
+        next(new Error(`No posts by autor ${username}...`));
+      } else {
+        res.status(200).json(postsArray);
+      }
+    })
+    .catch((error) => next(error));
+};
--- back-end/routers/relationships.js
+++ back-end/routers/relationships.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 const { getAllUsers } = require('../controller/users');
-const { getAllPosts } = require('../controller/posts');
+const { getAllPosts, getPostsByUserName } = require('../controller/posts');
 
 /* ---------- GET ---------- */
 router.get('/user', getAllUsers); //Get all users objects
 router.get('/post', getAllPosts); //Get all posts
+router.get('/post/:username', getPostsByUserName); //Get posts by userName
 
 module.exports = router;
