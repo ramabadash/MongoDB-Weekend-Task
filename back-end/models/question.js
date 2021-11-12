9710e7fc999b38966b2e12c18b11acc5b86663da
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-12 22:57:05 +0200

    # Create Question Schema

---
+++ back-end/models/question.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const QuestionSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  correctAnswer: {
+    type: String,
+    required: true,
+  },
+  answers: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  difficulty: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Question = mongoose.model('Question', QuestionSchema);
+
+module.exports = Question;
