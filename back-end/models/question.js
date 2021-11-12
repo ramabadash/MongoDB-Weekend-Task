580e87677bdb795562ccacd80503040fc29b7461
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-13 00:38:47 +0200

    # Add built-in trim validator on mongoose schema of Question

--- back-end/models/question.js
+++ back-end/models/question.js
@@ -4,20 +4,24 @@ const QuestionSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
+    trim: true,
   },
   correctAnswer: {
     type: String,
     required: true,
+    trim: true,
   },
   answers: [
     {
       type: String,
       required: true,
+      trim: true,
     },
   ],
   difficulty: {
     type: Number,
     required: true,
+    trim: true,
   },
 });
 
